23c8991c7ceea0cc77dadcb0860e35b2a07a5aab
Mustafa <79022605+mustafafiliz@users.noreply.github.com>
2023-09-02 15:05:11 +0300

    initial commit

---
+++ src/components/Divider.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+type Props = {
+  color?: string
+  height?: number
+  marginTop?: number
+  marginBottom?: number
+}
+
+const Divider = ({ color = "#ddd", height = 1, marginTop = 10, marginBottom = 10 }: Props) => {
+  return (
+    <div style={{ color, height, marginTop, marginBottom }} />
+  )
+}
+
+export default Divider
